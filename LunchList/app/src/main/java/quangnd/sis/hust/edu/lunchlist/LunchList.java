73db26793c9a09d11e5806a9075ebaf45cebba7a
quangnd <quangnd@qndev.net.fpt>
2019-02-13 10:19:09 +0700

    Tutorial 02 A Simple Form

---
+++ LunchList/app/src/main/java/quangnd/sis/hust/edu/lunchlist/LunchList.java
@@ -0,0 +1,13 @@
+package quangnd.sis.hust.edu.lunchlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LunchList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ LunchList/app/src/main/java/quangnd/sis/hust/edu/lunchlist/Restaurant.java
@@ -0,0 +1,23 @@
+package quangnd.sis.hust.edu.lunchlist;
+
+public class Restaurant {
+
+    private String name="";
+    private String address="";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
